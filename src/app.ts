7d8a857ef552a1b98625c21050d4fa0e57fa5686
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2022-05-09 22:20:15 +0600

    cors localhost

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import fastify from "fastify";
 import router from "./router";
 
-
 const server = fastify({
   // Logger only for production
   logger: !!(process.env.NODE_ENV !== "development"),
+});
+
+// CORS
+server.register(require("@fastify/cors"), {
+  origin: (origin: any, cb: any) => {
+    const hostname = new URL(origin).hostname;
+    if (hostname === "localhost") {
+      //  Request from localhost will pass
+      cb(null, true);
+      return;
+    }
+    // Generate an error on other origins, disabling access
+    cb(new Error("Not allowed"));
+  },
 });
 
 // Middleware: Router
